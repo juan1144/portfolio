e0249c25e8e6d86c84af26898b70b0cd54a11e6b
Juan Hernández <jfph25@gmail.com>
2025-04-13 16:13:40 -0600

    Adding custom fonts and colors, config tailwind to use it

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./templates/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        light: {
+          background: '#ffffff',
+          primary: '#262629',
+          secondary: '#6c6c75',
+          border: '#27272a',
+          accent: '#14b8a6',
+        },
+        dark: {
+          background: '#191919',
+          primary: '#f3f3f4',
+          secondary: '#93939c',
+          border: '#27272a',
+          accent: '#14b8a6',
+        },
+      },
+    },
   },
+  darkMode: 'class',
   plugins: [],
 }
