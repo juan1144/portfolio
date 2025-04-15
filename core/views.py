98386aace356b08eeffb93137842bd515966979b
Juan Hernández <jfph25@gmail.com>
2025-04-14 20:33:02 -0600

    Refactor work section and skills on home page

--- core/views.py
+++ core/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "core/home.html")
+    return render(
+        request,
+        "core/home.html",
+        {
+            "techs_frontend": [
+                "HTML",
+                "CSS",
+                "Bootstrap",
+                "Tailwind",
+                "HTMX",
+                "JQuery",
+                "JavaScript",
+            ],
+            "techs_backend": ["Python", "C#"],
+            "techs_db": ["PostgreSQL", "SQL", "MySQL"],
+        },
+    )
 
 
 def projects(request):
