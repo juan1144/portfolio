3c32e479a91f5dccfa1b78c6ec5c78fb9d32039c
Juan Hernández <jfph25@gmail.com>
2025-04-19 09:54:05 -0600

    Fixing media file to store images

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from typing import cast
+
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, URLPattern
+
+from config import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += cast(
+        list[URLPattern], static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )  # type: ignore[arg-type]
--- core/views.py
+++ core/views.py
@@ -6,36 +6,6 @@ from django.shortcuts import render, get_object_or_404
 from core.models import Profile
 
 
-# def home(request):
-#     init_date = date(2023, 5, 25)
-#     today = date.today()
-#
-#     experience_years = (
-#         today.year
-#         - init_date.year
-#         - ((today.month, today.day) < (init_date.month, init_date.day))
-#     )
-#
-#     return render(
-#         request,
-#         "core/home.html",
-#         {
-#             "techs_frontend": [
-#                 "HTML",
-#                 "CSS",
-#                 "Bootstrap",
-#                 "Tailwind",
-#                 "HTMX",
-#                 "JQuery",
-#                 "JavaScript",
-#             ],
-#             "techs_backend": ["Python", "C#"],
-#             "techs_db": ["PostgreSQL", "SQL", "MySQL"],
-#             "experience_years": experience_years,
-#         },
-#     )
-
-
 def home(request):
     profile = get_object_or_404(Profile, id=1)
 
@@ -43,13 +13,23 @@ def home(request):
     for skill in profile.skills.all():
         skills_by_category[skill.category].append(skill.name)
 
+    today = datetime.now()
+    experience_time = (
+        today.year
+        - profile.start_date.year
+        - (
+            (today.month, today.day)
+            < (profile.start_date.month, profile.start_date.day)
+        )
+    )
+
     context = {
         "profile": profile,
         "biographies": profile.biographies.all(),
         "socials": profile.socials.all(),
         "skills_by_category": dict(skills_by_category),
         "works": profile.works.all(),
-        "experience_time": datetime.now().year - profile.start_date.year,
+        "experience_time": experience_time,
     }
     return render(request, "core/home.html", context)
 
