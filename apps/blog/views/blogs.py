3590e758c60dadbc3264c5ad28c24dbcab89875d
Juan Hernández <jfph25@gmail.com>
2025-06-15 20:31:17 -0600

    Add search button to blogs and clear filter buttom

--- apps/blog/views/blogs.py
+++ apps/blog/views/blogs.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.utils.translation import get_language
 
@@ -5,12 +6,20 @@ from apps.blog.models import BlogsPost
 
 
 def blog(request):
-    """Render view for the blog page."""
+    """Renderiza el listado de entradas del blog, con filtro por búsqueda opcional."""
     language = get_language()
+    query = request.GET.get("q", "").strip()
+
     posts = BlogsPost.objects.filter(is_published=True, language=language)
 
+    has_filters = False
+    if query:
+        posts = posts.filter(Q(title__icontains=query) | Q(summary__icontains=query))
+        has_filters = True
+
     context = {
-        "posts": posts,
         "language": language,
+        "posts": posts,
+        "has_filters": has_filters,
     }
     return render(request, "blogs/blogs_list.html", context)
